cfb27fe69e653c787c5eb31f4fe2c1ca4ef3a344
Vivek kumar <vivek3553vk@gmail.com>
2024-04-11 00:01:56 +0530

    "Some development in frontend and added a backend"

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { useNavigate } from 'react-router-dom';
+// import { useLogoutMutation } from '../slices/usersApiSlice';
+// import { logout } from '../slices/authSlice';
+// import SearchBox from './SearchBox';
+import logo from '../assets/logo.png';
+// import { resetCart } from '../slices/cartSlice';
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg='primary' variant='dark' expand='md' collapseOnSelect>
+                <Container>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>
+                            <img src={logo} alt='ProShop' />
+                            ProShop
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='ms-auto'>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link>
+                                    <FaShoppingCart /> Cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                <Nav.Link>
+                                    <FaUser /> Sign In
+                                </Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header;
